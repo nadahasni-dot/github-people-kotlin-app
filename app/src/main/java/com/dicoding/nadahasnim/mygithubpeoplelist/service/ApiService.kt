0c230a0f152049956cb330b2e8866afedbd2ee07
Nada Hasni <nadasthing@gmail.com>
2022-05-31 12:28:57 +0700

    update github token

--- app/src/main/java/com/dicoding/nadahasnim/mygithubpeoplelist/service/ApiService.kt
+++ app/src/main/java/com/dicoding/nadahasnim/mygithubpeoplelist/service/ApiService.kt
@@ -9,35 +9,35 @@ import retrofit2.http.*
 
 interface ApiService {
     @GET("users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getAllUsers(): Call<List<ResponseListUsersItem>>
 
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun searchUsers(
         @Query("q") query: String
     ): Call<ResponseSearchUsers>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getDetailUser(
         @Path("username") username: String
     ): Call<ResponseDetailUser>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowers(
         @Path("username") username: String
     ): Call<List<ResponseListUsersItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowing(
         @Path("username") username: String
     ): Call<List<ResponseListUsersItem>>
 
     @GET("users/{username}/repos")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getRepositories(
         @Path("username") username: String
     ): Call<List<ResponseRepositories>>
