13b50280e9d4e4b67699b4bcc45631e5d47e2da5
Nada Hasni <nadasthing@gmail.com>
2022-06-24 13:40:59 +0700

    configure github token globally

--- app/src/main/java/com/dicoding/nadahasnim/mygithubpeoplelist/service/ApiService.kt
+++ app/src/main/java/com/dicoding/nadahasnim/mygithubpeoplelist/service/ApiService.kt
@@ -1,5 +1,6 @@
 package com.dicoding.nadahasnim.mygithubpeoplelist.service
 
+import com.dicoding.nadahasnim.mygithubpeoplelist.BuildConfig
 import com.dicoding.nadahasnim.mygithubpeoplelist.model.ResponseDetailUser
 import com.dicoding.nadahasnim.mygithubpeoplelist.model.ResponseListUsersItem
 import com.dicoding.nadahasnim.mygithubpeoplelist.model.ResponseRepositories
@@ -9,35 +10,35 @@ import retrofit2.http.*
 
 interface ApiService {
     @GET("users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getAllUsers(): Call<List<ResponseListUsersItem>>
 
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun searchUsers(
         @Query("q") query: String
     ): Call<ResponseSearchUsers>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getDetailUser(
         @Path("username") username: String
     ): Call<ResponseDetailUser>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getFollowers(
         @Path("username") username: String
     ): Call<List<ResponseListUsersItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getFollowing(
         @Path("username") username: String
     ): Call<List<ResponseListUsersItem>>
 
     @GET("users/{username}/repos")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getRepositories(
         @Path("username") username: String
     ): Call<List<ResponseRepositories>>
--- app/src/main/java/com/dicoding/nadahasnim/mygithubpeoplelist/viewmodel/DetailViewModelFactory.kt
+++ app/src/main/java/com/dicoding/nadahasnim/mygithubpeoplelist/viewmodel/DetailViewModelFactory.kt
@@ -5,7 +5,11 @@ import androidx.lifecycle.ViewModelProvider
 
 @Suppress("UNCHECKED_CAST")
 class DetailViewModelFactory(private val username: String) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+//    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+//
+//    }
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return DetailViewModel(username) as T
     }
 }
